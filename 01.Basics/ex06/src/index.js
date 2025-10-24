1fc4fcfcb88ffefbb15d5a93cb8693395f79d342
ri1700 <hazard3841@gmail.com>
2025-10-24 11:01:35 +0900

    jsx

--- 01.Basics/ex06/src/index.js
+++ 01.Basics/ex06/src/index.js
@@ -2,4 +2,4 @@ import ReactDom from 'react-dom/client';
 import {App} from './app.js';
 
 const root = ReactDom.createRoot(document.getElementById('root'));
-root.render(App());
+root.render(<App />);
