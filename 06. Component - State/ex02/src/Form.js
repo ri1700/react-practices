4d0ed54e148340aa1b1a1334fe94db345f664369
ri1700 <hazard3841@gmail.com>
2025-10-29 16:37:04 +0900

    add

--- 06. Component - State/ex02/src/Form.js
+++ 06. Component - State/ex02/src/Form.js
@@ -2,15 +2,61 @@ import React, { useState } from 'react';
 import './assets/Form.css';
 
 export default function Form() {
-
-
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [validEmail, setValidEmail] = useState(false);
+    const [birthYear, setBirthYear] = useState('');
+ 
     return (
-        <form id="joinForm" name="joinForm" method="post" action="/do/not/post">
+        <form 
+            id="joinForm" 
+            name="joinForm" 
+            method="post" 
+            action="/do/not/post"
+            onSubmit = {(e) => {
+                e.preventDefault();
+
+                console.log({
+                    name : name,
+                    email: email,
+                    birthYear: e.target.value
+                })
+            }}>
             <label htmlFor="name">이름</label>
-            <input id="name" name="name" type="text" />
+            <input 
+                id="name" 
+                name="name" 
+                type="text" 
+                value ={name}
+                onChange = {(e) => {
+                    // 글자 10자 제안
+                    setName(e.target.value.substring(0,10));
+                }}/>
 
             <label htmlFor="email">이메일</label>
-            <input id="email" name="email" type="text" value={ "" }/>
+            <input 
+                id="email" 
+                name="email" 
+                type="text" 
+                value={email}
+                onChange = {(e) => {
+                    setEmail(e.target.value);
+
+                    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+                    const valid = re.test(e.target.value);
+                    setValidEmail(valid);
+
+                }}/>
+
+            <span>
+                 {
+                    email ===''?
+                        null:
+                        validEmail ? 
+                            <b>{'O'}</b> : 
+                            <b>{'X'}</b>
+                 }   
+            </span>
 
             <label htmlFor="password">패스워드</label>
             <input id="password" name="password" type="password" value={ "" } />
@@ -22,7 +68,9 @@ export default function Form() {
             </fieldset>
 
             <label htmlFor="birthYear">생년</label>
-            <select id="birthYear">
+            <select 
+                id="birthYear" 
+                naem = "birthYear">
                 <option value='1984'>1984년</option>
                 <option value='1985'>1985년</option>
                 <option value='1986'>1986년</option>
